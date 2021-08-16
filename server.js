3d3187565f37ea91a29778602c0d38008835bcc3
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2021-08-16 23:45:09 +0100

    Upload code that sends the file to uploads folder

--- server.js
+++ server.js
@@ -10,9 +10,20 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+/* Upload */
 app.post('/', (req, res) =>{
     if(req.files){
-        console.log(req.files)
+        var file = req.files.file;
+        var filename = file.name;
+        console.log(filename);
+        
+        file.mv('./uploads/' + filename, function (err){ //192.168.0.X./uploads?
+            if(err){
+                res.send(err)
+            }else{
+                res.send("file uploaded with sucess")
+            }
+        });
     }
 })
 
